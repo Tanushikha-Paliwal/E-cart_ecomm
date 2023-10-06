77fd90dd145cac397e28cd5a7308ea6e9f77f814
Tanu <tanupaliwal2001@gmail.com>
2023-10-06 22:15:22 +0530

    product details added

--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -5,7 +5,8 @@ from app import views
 from .views import *
 urlpatterns = [
     path('', ProductView.as_view() , name="home" ),
-    path('product-detail/', views.product_detail, name='product-detail'),
+    path("product-detail/<int:pk>/" , ProductDetailView.as_view() , name="product-detail"),
+    # path('product-detail/', views.product_detail, name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
